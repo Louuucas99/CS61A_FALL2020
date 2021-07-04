3cc24668ce5f58b1e6f50fd86d9b822cf43a4ca4
Louuucas99 <61459093+Louuucas99@users.noreply.github.com>
2021-07-04 17:41:54 +0800

    Add files via upload

---
+++ Discussion/disc07.py
@@ -0,0 +1,21 @@
+#1.1
+class Student:
+    students = 0 # this is a class attribute
+    def __init__(self, name, staff):
+        self.name = name # this is an instance attribute
+        self.understanding = 0
+        Student.students += 1
+        print("There are now", Student.students, "students")
+        staff.add_student(self)
+    def visit_office_hours(self, staff):
+        staff.assist(self)
+        print("Thanks, " + staff.name)
+
+class Professor:
+    def __init__(self, name):
+        self.name = name
+        self.students = {}
+    def add_student(self, student):
+        self.students[student.name] = student
+    def assist(self, student):
+        student.understanding += 1
