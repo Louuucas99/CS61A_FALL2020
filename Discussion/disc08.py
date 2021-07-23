6bfcf46a4d20b17ab4a3fb79577739ce1c54448a
Louuucas99 <61459093+Louuucas99@users.noreply.github.com>
2021-07-24 01:13:03 +0800

    Update disc08.py

--- Discussion/disc08.py
+++ Discussion/disc08.py
@@ -93,11 +93,13 @@ def square_tree(t):
 
 #3.3
 def find_paths(t, entry):
+    """
     >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
     >>> find_paths(tree_ex, 5)
     [[2, 7, 6, 5], [2, 1, 5]]
     >>> find_paths(tree_ex, 12)
     []
+    """
     paths = []
     if t.label == entry:
         path.append([t.label])
